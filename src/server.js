a45df2fa66c13748b299a579392b4c81f9eb568d
anindyoo <anindyo0216@gmail.com>
2023-11-21 21:31:49 +0700

    feat: initial commit.

---
+++ src/server.js
@@ -0,0 +1,13 @@
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+};
