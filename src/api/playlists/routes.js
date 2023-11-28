3674cb88c0a281339e4be24916f9d78f5b69d8ab
anindyoo <anindyo0216@gmail.com>
2023-11-28 22:04:16 +0700

    feat: add routes for playlist songs handler.

--- src/api/playlists/routes.js
+++ src/api/playlists/routes.js
@@ -23,30 +23,30 @@ const routes = (handler) => [
       auth: 'openmusic_jwt',
     },
   },
-  // {
-  //   method: 'POST',
-  //   path: '/playlists/{id}/songs',
-  //   handler: handler.postPlaylistSongHandler,
-  //   options: {
-  //     auth: 'openmusic_jwt',
-  //   },
-  // },
-  // {
-  //   method: 'GET',
-  //   path: '/playlists/{id}/songs',
-  //   handler: handler.getPlaylistSongsHandler,
-  //   options: {
-  //     auth: 'openmusic_jwt',
-  //   },
-  // },
-  // {
-  //   method: 'POST',
-  //   path: '/playlists/{id}/songs',
-  //   handler: handler.deletePlaylistSongByIdHandler,
-  //   options: {
-  //     auth: 'openmusic_jwt',
-  //   },
-  // },
+  {
+    method: 'POST',
+    path: '/playlists/{id}/songs',
+    handler: handler.postPlaylistSongHandler,
+    options: {
+      auth: 'openmusic_jwt',
+    },
+  },
+  {
+    method: 'GET',
+    path: '/playlists/{id}/songs',
+    handler: handler.getPlaylistSongsHandler,
+    options: {
+      auth: 'openmusic_jwt',
+    },
+  },
+  {
+    method: 'DELETE',
+    path: '/playlists/{id}/songs',
+    handler: handler.deletePlaylistSongByIdHandler,
+    options: {
+      auth: 'openmusic_jwt',
+    },
+  },
 ];
 
 module.exports = routes;
