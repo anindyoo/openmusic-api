cbc2b3ff2ae0b8feade3a1d835e6d3ffd8d4dc43
anindyoo <anindyo0216@gmail.com>
2023-11-29 21:23:43 +0700

    feat: add collaborations migration.

---
+++ migrations/1701259240194_create-table-collaborations.js
@@ -0,0 +1,28 @@
+/* eslint-disable camelcase */
+
+exports.up = (pgm) => {
+  pgm.createTable('collaborations', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    playlist_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      onDelete: 'cascade',
+      referencesConstraintName: 'fk_collaborations.playlist_id_playlists.id',
+    },
+    user_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      onDelete: 'cascade',
+      referencesConstraintName: 'fk_collaborations.user_id_users.id',
+    },
+  });
+
+  pgm.addConstraint('collaborations', 'unique_playlist_id_and_user_id', 'UNIQUE(playlist_id, user_id)');
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('collaborations');
+};
