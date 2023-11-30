af0d9d7843d3d784d1a8e39c0ea572bdd9747bb1
anindyoo <anindyo0216@gmail.com>
2023-12-01 00:19:41 +0700

    feat: add playlist song activities migration.

---
+++ migrations/1701351891200_create-table-playlist-song-activities.js
@@ -0,0 +1,43 @@
+/* eslint-disable camelcase */
+
+exports.up = (pgm) => {
+  pgm.createTable('playlist_song_activities', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    playlist_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      references: 'playlists',
+      onDelete: 'cascade',
+      referencesConstraintName: 'fk_playlist_song_activities.playlist_id_playlists.id',
+    },
+    song_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      references: 'songs',
+      onDelete: 'cascade',
+      referencesConstraintName: 'fk_playlist_song_activities.song_id_songs.id',
+    },
+    user_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      references: 'users',
+      onDelete: 'cascade',
+      referencesConstraintName: 'fk_playlist_song_activities.user_id_users.id',
+    },
+    action: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    time: {
+      type: 'TEXT',
+      notNull: true,
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('playlist_song_activities');
+};
