d4c7f8436eb485607152403f41a700802ebdaf1e
anindyoo <anindyo0216@gmail.com>
2023-12-01 00:20:20 +0700

    feat: add functions for adding and getting playlist song activities.

--- src/services/playlists/PlaylistsService.js
+++ src/services/playlists/PlaylistsService.js
@@ -159,6 +159,49 @@ class PlaylistsService {
       }
     }
   }
+
+  async addPlaylistActivity({
+    playlistId, songId, userId, action,
+  }) {
+    const id = `playlist-song-activity-${nanoid(16)}`;
+    const time = new Date().toISOString();
+    const query = {
+      text: 'INSERT INTO playlist_song_activities VALUES($1, $2, $3, $4, $5, $6) RETURNING id',
+      values: [id, playlistId, songId, userId, action, time],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows[0].id) {
+      throw new InvariantError('Playlist Activity gagal ditambahkan.');
+    }
+
+    return result.rows[0].id;
+  }
+
+  async getPlaylistActivities(playlistId) {
+    const query = {
+      // text: `
+      //   SELECT username, title, action, time
+      //   FROM playlist_song_activities
+      //   WHERE playlist_id = $1
+      // `,
+      text: `
+        SELECT users.username, songs.title, playlist_song_activities.action, playlist_song_activities.time
+        FROM playlist_song_activities 
+        INNER JOIN users
+          ON users.id = playlist_song_activities.user_id
+        INNER JOIN songs
+          ON songs.id = playlist_song_activities.song_id
+        WHERE playlist_song_activities.playlist_id = $1
+      `,
+      values: [playlistId],
+    };
+
+    const result = await this._pool.query(query);
+
+    return result.rows;
+  }
 }
 
 module.exports = PlaylistsService;
