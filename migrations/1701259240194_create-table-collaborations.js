3a2661ebebfd05f288eef173f8dd7dea74517cea
anindyoo <anindyo0216@gmail.com>
2023-11-30 15:13:20 +0700

    feat: add column references.

--- migrations/1701259240194_create-table-collaborations.js
+++ migrations/1701259240194_create-table-collaborations.js
@@ -9,12 +9,14 @@ exports.up = (pgm) => {
     playlist_id: {
       type: 'VARCHAR(50)',
       notNull: true,
+      references: 'playlists',
       onDelete: 'cascade',
       referencesConstraintName: 'fk_collaborations.playlist_id_playlists.id',
     },
     user_id: {
       type: 'VARCHAR(50)',
       notNull: true,
+      references: 'users',
       onDelete: 'cascade',
       referencesConstraintName: 'fk_collaborations.user_id_users.id',
     },
